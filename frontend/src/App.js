ae8d7421da3eb5b4496b30fe81d6b49157c37be4
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-23 19:30:36 +0300

    L-389. Implement auth action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication.js'
+import AuthenticationPage, { action as authAction } from './pages/Authentication.js'
 
 const router = createBrowserRouter([
   {
@@ -57,7 +57,8 @@ const router = createBrowserRouter([
       },
       {
         path: 'auth',
-        element: <AuthenticationPage />
+        element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: 'newsletter',
