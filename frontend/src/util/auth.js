03a626d6d60171426ac4fd509b5adf7a8b75847b
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-25 20:22:43 +0300

    L-397, 398. Manage token expiration and logout user after 1 hour, as set in server

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,26 @@
 import { redirect } from "react-router-dom";
 
+/**
+ * @returns duration that token is still active. If duration < 0, token is expired. If duration > 0, token is still valid.
+ */
+export function getTokenDuration() {
+    const expirationDate = new Date(localStorage.getItem('expiration'));
+    return expirationDate.getTime() - new Date().getTime(); // expiration time - now. If
+}
+
 export function getAuthToken() {
-    return localStorage.getItem('token');
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        return null;
+    }
+
+    const tokenDuration = getTokenDuration();
+    if (tokenDuration < 0) {
+        return 'EXPIRED';
+    }
+
+    return token;
 }
 
 export function tokenLoader() {
